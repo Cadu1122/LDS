03783023e5da84cf43456abeb19df2718aad9488
GabrielRbrMendonca <83738265+GabrielRbrMendonca@users.noreply.github.com>
2022-11-13 20:31:23 -0300

    Controllers atualizados Aluno e Professor

--- Lab_03/Implementacao/moeda_estudantil/src/main/java/com/moeda_estudantil/Controllers/ProfessorController.java
+++ Lab_03/Implementacao/moeda_estudantil/src/main/java/com/moeda_estudantil/Controllers/ProfessorController.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import com.moeda_estudantil.Models.HistoricoDAO;
 import com.moeda_estudantil.Models.ProfessorDAO;
+
 
 @Controller
 @RequestMapping(path = "/professores")
@@ -24,6 +27,19 @@ public class ProfessorController {
     public ModelAndView getHistorico(@PathVariable String login) {
         ModelAndView modelAndView = new ModelAndView("Professores/historico");
         modelAndView.addObject("historicos", HistoricoDAO.getInstance().encontrar(ProfessorDAO.encontrar(login)));
+        return modelAndView;
+    }
+
+    @PostMapping(path = "/{login}/moedas")
+    public ModelAndView giveCoins(@PathVariable String login, String loginAluno, int qte, String motivo) {
+            ProfessorDAO.getInstance().darMoedas(login, loginAluno, qte, motivo);
+            return new ModelAndView("redirect:/alunos");      
+    }
+
+    @GetMapping(path = "/{login}/moedas")
+    public ModelAndView getPagMoedas(@PathVariable String login ) {
+        ModelAndView modelAndView = new ModelAndView("Professores/moedas");
+        modelAndView.addObject("login", login);
         return modelAndView;
     }
 }
